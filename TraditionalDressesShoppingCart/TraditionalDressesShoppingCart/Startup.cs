5f8df7c27cf74fd2558e561d64d5f7a0a37da776
BS <bbstha143@gmail.com>
2017-07-26 17:19:47 +0930

    initial commit by bishal

---
+++ TraditionalDressesShoppingCart/TraditionalDressesShoppingCart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TraditionalDressesShoppingCart.Startup))]
+namespace TraditionalDressesShoppingCart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
