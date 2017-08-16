61a8e4e5a3e91136a2bfda1125e81fcdb455c6a0
LAPTOP-FHCS8DPP\SK <kanchankaushalz@yahoo.co.in>
2017-08-16 15:47:21 +0930

    new student project

---
+++ demo3/Controllers/NewHomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo3.Controllers
+{
+    public class NewHomeController : Controller
+    {
+        // GET: NewHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Goto()
+        {
+            return View("Sharebrain");
+
+        }
+    }
+}
